5beb22b8d4c8b824f7ba5f6d991438fbd96c5337
Renan Baia <renanbaiar@gmail.com>
2020-01-24 18:38:14 -0300

    Start send email notification

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -8,7 +8,7 @@ class ScheduleController {
     const checkUserProvider = await User.findOne({
       where: { id: req.userId, provider: true },
     });
-
+    
     if (!checkUserProvider) {
       return res.status(401).json({ error: 'User is not a provider' });
     }
